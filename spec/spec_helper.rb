62e3a3d0c4c0852fb9dfbdf5f90a4338c44d4fb6
Phuoc <onhachungphuoc@gmail.com>
2017-07-20 14:14:15 +0700

    fix rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,13 +16,6 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-require 'simplecov'
-SimpleCov.start 'rails'
-require 'simplecov-json'
-SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
-require 'coveralls'
-Coveralls.wear!
-
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
